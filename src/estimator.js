b1edd044afe4808dc112d69a966e2e20a30437a8
Oluwajoba <oluwajobabello@gmail.com>
2020-04-10 17:17:33 +0100

    check challenge 1 / .estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -4,7 +4,7 @@
 //     currentlyInfected: data.reportedCases * 10,
 //     infectionsByRequestedTime: data.reportedCases * 10 * 1024
 //   },
-//   severeImpact: {
+//   sevImpact: {
 //     currentlyInfected: data.reportedCases * 50,
 //     infectionsByRequestedTime: data.reportedCases * 10 * 1024
 //   }
@@ -12,20 +12,32 @@
 function covid19ImpactEstimator(data) {
   console.log(data);
   console.log(data.reportedCases);
-  const currentlyInfected1 = data.reportedCases * 10;
-  const currentlyInfected2 = data.reportedCases * 50;
-  const infectionsByRequestedTime1 = currentlyInfected1 * 7;
-  const infectionsByRequestedTime2 = currentlyInfected2 * 7;
+  let calImpCurrentlyInfected;
+  let calSevImpCurrentlyInfected;
+  let calImpInfectionsByRequestedTime;
+  let calSevImpInfectionsByRequestedTime;
+
+  const impCurrentlyInfected = data.reportedCases * 10;
+  const sevImpCurrentlyInfected = data.reportedCases * 50;
+  const impInfectionsByRequestedTime = impCurrentlyInfected * 512;
+  const sevImpInfectionsByRequestedTime = sevImpCurrentlyInfected * 512;
+
+  if (data.periodType === 'days') {
+    calImpCurrentlyInfected = impCurrentlyInfected * 7;
+    calSevImpCurrentlyInfected = sevImpCurrentlyInfected * 7;
+    calImpInfectionsByRequestedTime = impInfectionsByRequestedTime * 7;
+    calSevImpInfectionsByRequestedTime = sevImpInfectionsByRequestedTime * 7;
+  }
 
   return {
     data,
     impact: {
-      currentlyInfected: currentlyInfected1,
-      infectionsByRequestedTime: infectionsByRequestedTime1
+      currentlyInfected: calImpCurrentlyInfected,
+      infectionsByRequestedTime: calImpInfectionsByRequestedTime
     },
     severeImpact: {
-      currentlyInfected: currentlyInfected2,
-      infectionsByRequestedTime: infectionsByRequestedTime2
+      currentlyInfected: calSevImpCurrentlyInfected,
+      infectionsByRequestedTime: calSevImpInfectionsByRequestedTime
     }
   };
 }
