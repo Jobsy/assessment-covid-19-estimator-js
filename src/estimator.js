23182ffd3d8facd21e58d76859a92a1e3318c0af
Oluwajoba <oluwajobabello@gmail.com>
2020-04-10 12:41:04 +0100

    add challenge  1 / check for errors / estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,30 @@
 const covid19ImpactEstimator = (data) => {
-  console.log(data, 'testing');
+  //   console.log(data, 'testing');
+  return {
+    data: data,
+    impact: {
+      currentlyInfested: data.data.reportedCases * 10,
+      infectionByRequestedTime: impact.currentlyInfested * 1024
+    },
+    severeImpact: {
+      currentlyInfested: data.data.reportedCases * 50,
+      infectionByRequestedTime: impact.currentlyInfested * 1024
+    }
+  };
 };
 
 export default covid19ImpactEstimator;
+
+// {
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 1,
+//       avgDailyIncomePopulation: 0.75
+//     },
+//     reportedCases: 1294,
+//     population: 8300519,
+//     totalHospitalBeds: 184315,
+//     timeToElapse: 59,
+//     periodType: 'days'
+//   }
